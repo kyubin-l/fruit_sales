1eb49ac6147f16e1dcd0bb5a2177f9fa8acd16a8
kyubin-l <lee.kyubin2176@gmail.com>
2022-11-01 12:07:52 +0000

    urls and views changes

--- fruit_sales/urls.py
+++ fruit_sales/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import shops.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', shops.views.home, name='home'),
+
 ]
