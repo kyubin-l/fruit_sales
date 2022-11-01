aaadee46a6a35fc4f08e81e9d11b504d8606254d
kyubin-l <lee.kyubin2176@gmail.com>
2022-11-01 11:32:14 +0000

    minor changes in prompting user

--- shops/management/commands/import_shops_and_cities.py
+++ shops/management/commands/import_shops_and_cities.py
@@ -13,9 +13,8 @@ class Command(BaseCommand):
         pass
     
     def handle(self, *args, **kwargs):
-        # delete_data = input('You are about to delete all existing shops and cities, are you sure? (y/n) ')
-        # if delete_data == 'y':
-        if True:
+        delete_data = input('You are about to delete all existing shops and cities, are you sure? (YES/Y to confirm, any other key to quit) ')
+        if delete_data.lower() in ['y', 'yes']:
             self.stdout.write('Continuing to delete')
             self.stdout.write(f'Deleting {Shop.objects.count()} Shop objects')
             Shop.objects.all().delete()
@@ -25,6 +24,8 @@ class Command(BaseCommand):
             data.apply(create_objects, axis=1)
             self.stdout.write(f'Created {City.objects.count()} City objects')
             self.stdout.write(f'Created {Shop.objects.count()} Shop objects')
+        else:
+            self.stdout.write('Exiting')
         
 
 def import_data():
@@ -38,8 +39,9 @@ def create_objects(row):
         city=city,
         name=row['City'],
         code=row['Code'],
-        address='tmp',
-        postcode=row['Code'],
+        # address='tmp',
+        # postcode=row['Code'],
+        year_opened=2010
     )
     shop.save()
     
--- shops/models.py
+++ shops/models.py
@@ -14,10 +14,10 @@ class Shop(models.Model):
     city = models.ForeignKey(City, on_delete=models.PROTECT)
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=5)
-    address = models.CharField(max_length=100)
-    postcode = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    postcode = models.CharField(max_length=100, null=True, blank=True)
     year_opened = models.IntegerField(default=0)
 
 
     def __str__(self) -> str:
-        return f'{self.city}, {self.name}, {self.postcode}'
+        return f'{self.city}, {self.name}, {self.code}'
